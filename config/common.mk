96257474bd23c1a1cb3a7900918000f186c5911e
Luca Stefani <luca.stefani.ge1@gmail.com>
2019-07-25 18:05:40 +0200

    vendor: Switch back to enforce privapp-permissions whitelist
    Change-Id: I1ca080e5addcc0876d7346f3cf09db8e8af39312

--- config/common.mk
+++ config/common.mk
@@ -95,7 +95,7 @@ PRODUCT_COPY_FILES += \
 
 #Enable transitional log for Privileged permissions
 PRODUCT_PRODUCT_PROPERTIES += \
-    ro.control_privapp_permissions=log
+    ro.control_privapp_permissions=enforce
 
 ifneq ($(TARGET_DISABLE_LINEAGE_SDK), true)
 # Lineage SDK
