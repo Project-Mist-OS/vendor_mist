10c278ede423cb1398bf044932bee5e11a78bf4c
PainKiller3 <ninadpatil100@gmail.com>
2021-11-27 15:14:59 +0100

    vendor: Build Pixel charger animation conditionally
    Change-Id: I03744f039cb7eed579a6d489becf6ce22f804765
    Signed-off-by: PainKiller3 <ninadpatil100@gmail.com>
    Signed-off-by: S I P U N <sipunkumar85@gmail.com>

--- config/packages.mk
+++ config/packages.mk
@@ -38,8 +38,10 @@ PRODUCT_PACKAGE_OVERLAYS += vendor/mist/overlay/dictionaries
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/mist/overlay/dictionaries
 
 # Charger
+ifeq ($(USE_PIXEL_CHARGER),true)
 PRODUCT_PACKAGES += \
     product_charger_res_images
+endif
 
 # Extra tools in Mist
 PRODUCT_PACKAGES += \
