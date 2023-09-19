CherishOS/android_vendor_cherish

vendor: Add SettingsIntelligenceGooglePrebuilt
* From redfin-user-12-SP1A.210812.015

Signed-off-by: RasyidAlKautsar <rasyid2007alkautsar@gmail.com>

prebuilt/common/Android.mk
@@ -27,3 +27,16 @@ endif
 LOCAL_PREBUILT_MODULE_FILE := $(FINAL_APNS_FILE)
 
 include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := SettingsIntelligenceGooglePrebuilt
+LOCAL_OVERRIDES_PACKAGES := SettingsIntelligence
+LOCAL_MODULE_TAGS := optional
+LOCAL_SRC_FILES := priv-app/$(LOCAL_MODULE).apk
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_MODULE_CLASS := APPS
+LOCAL_PRIVILEGED_MODULE := true
+LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+LOCAL_DEX_PREOPT := false
+LOCAL_PRODUCT_MODULE := true
+include $(BUILD_PREBUILT)

Project-Mist-OS/vendor_mist

prebuilt/common/Android.mk
@@ -27,3 +27,16 @@ endif
 LOCAL_PREBUILT_MODULE_FILE := $(FINAL_APNS_FILE)
 
 include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := SettingsIntelligenceGoogle
+LOCAL_OVERRIDES_PACKAGES := SettingsIntelligence
+LOCAL_MODULE_TAGS := optional
+LOCAL_SRC_FILES := priv-app/$(LOCAL_MODULE).apk
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_MODULE_CLASS := APPS
+LOCAL_PRIVILEGED_MODULE := true
+LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+LOCAL_DEX_PREOPT := false
+LOCAL_PRODUCT_MODULE := true
+include $(BUILD_PREBUILT)