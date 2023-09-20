VoidUI-Tiramisu/vendor_aosp

vendor: qcom: Include qcom_boards.mk
* Can you believe it was missed?

Signed-off-by: electimon <electimon@gmail.com>

build/core/qcom_target.mk
@@ -27,4 +27,5 @@ else
 
 QC_OPEN_PATH := vendor/qcom/opensource
 
+include $(TOPDIR)vendor/aosp/build/core/vendor/qcom_boards.mk
 endif

Project-Mist-OS/vendor_mist

build/core/qcom_target.mk
@@ -27,4 +27,5 @@ else
 
 QC_OPEN_PATH := vendor/qcom/opensource
 
+include $(TOPDIR)vendor/mist/build/core/vendor/qcom_boards.mk
 endif