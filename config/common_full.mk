832098a3bd2e452f2153d4d995ff682c0088ca39
ShukakuZa <yusufzuzu787@gmail.com>
2023-10-26 18:33:18 +0000

    vendor: Initial rebrand to MistOS

--- config/common_full.mk
+++ config/common_full.mk
@@ -1,5 +1,5 @@
 # Inherit common Lineage stuff
-$(call inherit-product, vendor/lineage/config/common_mobile.mk)
+$(call inherit-product, vendor/mist/config/common_mobile.mk)
 
 PRODUCT_SIZE := full
 
