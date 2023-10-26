9af7d16550439d62194055cddebc94fa17eb6275
ShukakuZa <yusufzuzu787@gmail.com>
2023-10-26 20:06:42 +0000

    config: Exclude window_extensions addon on mini_tablet

--- config/common_mini_tablet.mk
+++ config/common_mini_tablet.mk
@@ -1,6 +1,5 @@
 # Inherit mini common Lineage stuff
 $(call inherit-product, vendor/mist/config/common_mini.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/window_extensions.mk)
 
 # Required packages
 PRODUCT_PACKAGES += \
