99b3aea207887206435687472ea89ef9c4433a5c
Michael Bestas <mkbestas@lineageos.org>
2023-10-26 19:31:05 +0000

    config: Remove TARGET_CAMERA_BOOTTIME_TIMESTAMP This is no longer needed in T

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -30,7 +30,6 @@ SOONG_CONFIG_NAMESPACES += mistGlobalVars
 SOONG_CONFIG_mistGlobalVars += \
     additional_gralloc_10_usage_bits \
     gralloc_handle_has_reserved_size \
-    needs_camera_boottime \
     target_init_vendor_lib \
     target_ld_shim_libs \
     target_surfaceflinger_udfps_lib \
@@ -56,7 +55,6 @@ endif
 
 # Soong bool variables
 SOONG_CONFIG_mistGlobalVars_gralloc_handle_has_reserved_size := $(TARGET_GRALLOC_HANDLE_HAS_RESERVED_SIZE)
-SOONG_CONFIG_mistGlobalVars_needs_camera_boottime := $(TARGET_CAMERA_BOOTTIME_TIMESTAMP)
 SOONG_CONFIG_mistGlobalVars_uses_egl_display_array := $(TARGET_USES_EGL_DISPLAY_ARRAY)
 SOONG_CONFIG_mistNvidiaVars_uses_nvidia_enhancements := $(NV_ANDROID_FRAMEWORK_ENHANCEMENTS)
 SOONG_CONFIG_mistQcomVars_supports_extended_compress_format := $(AUDIO_FEATURE_ENABLED_EXTENDED_COMPRESS_FORMAT)
