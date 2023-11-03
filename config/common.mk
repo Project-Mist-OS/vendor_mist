PixelExperience/vendor_aosp

vendor: Import clocks package
Change-Id: I0025fe6f12d68af8f4a8f4b256bd82822d37efdf

config/common.mk
@@ -219,6 +219,9 @@ $(call inherit-product, vendor/gms/products/gms.mk)
 # RRO Overlays
 $(call inherit-product, vendor/aosp/config/rro_overlays.mk)
 
+# Clocks
+$(call inherit-product, vendor/aosp/config/clocks.mk)
+
 # Pixel Framework
 #$(call inherit-product, vendor/pixel-framework/config.mk)
 

Project-Mist-OS/vendor_mist

Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

config/common.mk
@@ -356,6 +356,9 @@ USE_THINLTO_CACHE := true
 SKIP_ABI_CHECKS := true
 endif
 
+# Clocks
+$(call inherit-product, vendor/cherish/config/clocks.mk)
+
 # Pixel Framework
 $(call inherit-product-if-exists, vendor/pixel-framework/config.mk)
 