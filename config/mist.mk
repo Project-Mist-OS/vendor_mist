e65d56f46318a79bc546bcd9251009e3132f90d2
ponces <ponces26@gmail.com>
2023-11-15 16:02:38 +0000

    vendor: Switch to AOSPA's face unlock implementation
    Change-Id: I22f737d98ad6d21fac2fa0e1abd0ea002f05341b
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- config/mist.mk
+++ config/mist.mk
@@ -1,2 +1,14 @@
 PRODUCT_PACKAGES += \
     Updater
+
+# Face Unlock
+ifeq ($(TARGET_SUPPORTS_64_BIT_APPS),true)
+PRODUCT_PACKAGES += \
+    FaceUnlock
+
+PRODUCT_SYSTEM_EXT_PROPERTIES += \
+    ro.face.sense_service=true
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/android.hardware.biometrics.face.xml
+endif
