b99856930084efa9246dd374f1a7efda349c2663
aswin7469 <aswinas@pixysos.com>
2023-11-18 10:43:29 +0530

    evolution: Enable Cloned App
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Co-authored-by: John Galt <johngaltfirstrun@gmail.com>
    Co-authored-by: minaripenguin <minaripenguin@users.noreply.github.com>
    
    Signed-off-by: AnierinB <anierin@evolution-x.org>

--- config/mist.mk
+++ config/mist.mk
@@ -11,3 +11,7 @@ ifeq ($(WITH_GMS),true)
     endif
   endif
 endif
+
+# Cloned app exemption
+PRODUCT_COPY_FILES += \
+    vendor/lineage/prebuilt/common/etc/sysconfig/preinstalled-packages-platform-evolution-product.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/preinstalled-packages-platform-evolution-product.xml
