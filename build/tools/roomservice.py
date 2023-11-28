2e701a8f062af070c0a22c1bd4d5607a1d1c925d
ShukakuZa <yusufzuzu787@gmail.com>
2023-11-28 13:52:51 +0000

    vendor: Update roomservice to udc

--- build/tools/roomservice.py
+++ build/tools/roomservice.py
@@ -43,7 +43,7 @@ except ImportError:
 DEBUG = False
 
 custom_local_manifest = ".repo/local_manifests/pixel.xml"
-custom_default_revision =  os.getenv('ROOMSERVICE_DEFAULT_BRANCH', 'tiramisu')
+custom_default_revision =  os.getenv('ROOMSERVICE_DEFAULT_BRANCH', '14')
 custom_dependencies = "mist.dependencies"
 org_manifest = "mist-devices"  # leave empty if org is provided in manifest
 org_display = "MistOS-Devices"  # needed for displaying
