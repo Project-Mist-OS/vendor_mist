d6c3aeb5b8fb5d28f1eaee68b85394b266bffac7
cjh1249131356 <cjh1249131356@gmail.com>
2023-12-01 21:11:39 +0000

    soong: Add TARGET_USES_NOTHING_CAMERA Change-Id: Ib03ed29f9649750fa0f10f28615c109ded21b55a Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com> Signed-off-by: aswin7469 <aswinas@pixysos.com>

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -50,6 +50,7 @@ SOONG_CONFIG_mistGlobalVars += \
     target_trust_usb_control_enable \
     target_trust_usb_control_disable \
     uses_egl_display_array \
+    uses_nothing_camera \
     uses_oplus_camera
 
 SOONG_CONFIG_NAMESPACES += mistNvidiaVars
@@ -76,6 +77,7 @@ SOONG_CONFIG_mistGlobalVars_uses_egl_display_array := $(TARGET_USES_EGL_DISPLAY_
 SOONG_CONFIG_mistNvidiaVars_uses_nvidia_enhancements := $(NV_ANDROID_FRAMEWORK_ENHANCEMENTS)
 SOONG_CONFIG_mistQcomVars_supports_extended_compress_format := $(AUDIO_FEATURE_ENABLED_EXTENDED_COMPRESS_FORMAT)
 SOONG_CONFIG_mistQcomVars_uses_pre_uplink_features_netmgrd := $(TARGET_USES_PRE_UPLINK_FEATURES_NETMGRD)
+SOONG_CONFIG_mistGlobalVars_uses_nothing_camera := $(TARGET_USES_NOTHING_CAMERA)
 SOONG_CONFIG_mistGlobalVars_uses_oplus_camera := $(TARGET_USES_OPLUS_CAMERA)
 
 # Set default values
