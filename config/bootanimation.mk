f297b4d08b4f9c736d4fe54f14deb7d58ab9a0ca
ShukakuZa <yusufzuzu787@gmail.com>
2024-01-02 14:33:36 +0000

    vendor: Updated Pixel Bootanimation

--- config/bootanimation.mk
+++ config/bootanimation.mk
@@ -5,7 +5,7 @@ ifeq ($(TARGET_BOOT_ANIMATION_RES),1080)
 else ifeq ($(TARGET_BOOT_ANIMATION_RES),1440)
      PRODUCT_COPY_FILES += vendor/mist/bootanimation/bootanimation_1440.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
 else ifeq ($(TARGET_BOOT_ANIMATION_RES),720)
-     PRODUCT_COPY_FILES += vendor/mist/bootanimation/bootanimation_720.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
+     PRODUCT_COPY_FILES += vendor/mist/bootanimation/bootanimation_1080.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
 else
     ifeq ($(TARGET_BOOT_ANIMATION_RES),)
         $(warning "MistStyle: TARGET_BOOT_ANIMATION_RES is undefined, assuming 1080p")
