5c14c972c5818dd8f01d90432c8f30ae9792d9c3
rmp22 <195054967+rmp22@users.noreply.github.com>
2024-06-17 19:55:36 +0900

    config: add blur support
    Change-Id: I5fabdbbdfe812346f17dbba5d4bf9abe7344e0ca

--- config/mist.mk
+++ config/mist.mk
@@ -26,6 +26,20 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     BtHelper
 
+# Enable blur
+TARGET_ENABLE_BLUR ?= true
+ifeq ($(TARGET_ENABLE_BLUR),true)
+PRODUCT_SYSTEM_PROPERTIES += \
+    ro.custom.blur.enable=true \
+    persist.sysui.disableBlur=false \
+    ro.surface_flinger.supports_background_blur=1
+else
+PRODUCT_SYSTEM_PROPERTIES += \
+    ro.custom.blur.enable=false \
+    persist.sysui.disableBlur=true \
+    ro.surface_flinger.supports_background_blur=0
+endif
+
 # Face Unlock
 ifeq ($(TARGET_SUPPORTS_64_BIT_APPS),true)
 PRODUCT_PACKAGES += \
