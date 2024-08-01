ProjectEverest/vendor_everest

config: Add suport for Vanilla

config/common.mk
@@ -48,10 +48,12 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     frameworks/base/data/keyboards/Vendor_045e_Product_028e.kl:$(TARGET_COPY_OUT_PRODUCT)/usr/keylayout/Vendor_045e_Product_0719.kl
 
-# Gapps
+# Vanilla and GMS
 WITH_GAPPS ?= true
-ifeq ($(WITH_GAPPS),true)
+ifeq ($(WITH_GAPPS), true)
 $(call inherit-product-if-exists, vendor/gms/products/gms.mk)
+else
+include vendor/everest/config/vanilla.mk
 endif
 
 # Gboard side padding

Project-Mist-OS/vendor_mist

config/common.mk
@@ -62,10 +62,12 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.biometrics.face.xml
 endif
 
-# Gapps
+# Vanilla and GMS
 WITH_GAPPS ?= true
-ifeq ($(WITH_GAPPS),true)
+ifeq ($(WITH_GAPPS), true)
 $(call inherit-product-if-exists, vendor/gms/products/gms.mk)
+else
+include vendor/mist/config/vanilla.mk
 endif
 
 # Gboard side padding