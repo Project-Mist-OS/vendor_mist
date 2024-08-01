ProjectEverest/vendor_everest

vendor: Import Audios from RisingOS
* Add ringtones, notification and alarm tones

Signed-off-by: SharmagRit <sharmagritstuff@gmail.com>

config/common.mk
@@ -108,6 +108,9 @@ SYSTEMUI_OPTIMIZE_JAVA ?= true
 # Disable vendor restrictions
 PRODUCT_RESTRICT_VENDOR_FILES := false
 
+# Audio
+include vendor/everest/audio/audio.mk
+
 # Bootanimation
 include vendor/everest/config/bootanimation.mk
 

Project-Mist-OS/vendor_mist

config/common.mk
@@ -108,6 +108,9 @@ SYSTEMUI_OPTIMIZE_JAVA ?= true
 # Disable vendor restrictions
 PRODUCT_RESTRICT_VENDOR_FILES := false
 
+# Audio
+include vendor/mist/audio/audio.mk
+
 # Bootanimation
 include vendor/mist/config/bootanimation.mk
 