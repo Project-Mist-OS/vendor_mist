c0smic-Lab/vendor_aosp

build: Move around some props

config/version.mk
@@ -11,5 +11,12 @@ CUSTOM_PLATFORM_VERSION := 14.0
 CUSTOM_VERSION := cAOSP_$(CUSTOM_BUILD)-$(CUSTOM_PLATFORM_VERSION)-$(CUSTOM_BUILD_DATE)
 CUSTOM_VERSION_PROP := fourteen
 
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.custom.build.date=$(BUILD_DATE) \
+    ro.custom.device=$(CUSTOM_BUILD) \
+    ro.custom.fingerprint=$(ROM_FINGERPRINT) \
+    ro.custom.version=$(CUSTOM_VERSION) \
+    ro.modversion=$(CUSTOM_VERSION)
+
 # Signing
 -include vendor/lineage-priv/keys/keys.mk

Project-Mist-OS/vendor_mist

config/version.mk
@@ -11,5 +11,13 @@ MIST_PLATFORM_VERSION := 15.0
 
 MIST_VERSION := v$(MIST_DISPLAY_VERSION)-$(MIST_BUILD)-$(MIST_PLATFORM_VERSION)-$(MIST_BUILD_DATE)
 
+# MistOS Platform Version
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.mist.build.date=$(BUILD_DATE) \
+    ro.mist.device=$(MIST_BUILD) \
+    ro.mist.fingerprint=$(ROM_FINGERPRINT) \
+    ro.mist.version=$(MIST_VERSION) \
+    ro.modversion=$(MIST_VERSION)
+
 # Signing
 -include vendor/mist-priv/keys/keys.mk