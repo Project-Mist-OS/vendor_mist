ef632e20d4e11a46d114d8b1eb0e271b8b71e5ee
minaripenguin <minaripenguin@users.noreply.github.com>
2024-12-01 01:24:57 +0900

    config: Disable memtag for different processes
    * it defaults to async if not specified
    
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- config/mist.mk
+++ config/mist.mk
@@ -36,3 +36,10 @@ ifeq ($(TARGET_BUILD_DEVICE_AS_WEBCAM), true)
     PRODUCT_VENDOR_PROPERTIES += \
         ro.usb.uvc.enabled=true
 endif
+
+# Disable async MTE on a few processes
+PRODUCT_SYSTEM_EXT_PROPERTIES += \
+    persist.arm64.memtag.app.com.android.se=off \
+    persist.arm64.memtag.app.com.google.android.bluetooth=off \
+    persist.arm64.memtag.app.com.android.nfc=off \
+    persist.arm64.memtag.process.system_server=off
