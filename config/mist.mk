62c57ff209a6496e64734c178435d22d45932aba
minaripenguin <minaripenguin@users.noreply.github.com>
2025-06-26 07:16:59 +0000

    vendor: Add pihooks default values
    Change-Id: I4ab2c11b722bdb4e109733fdbdaab34fe644359a

--- config/mist.mk
+++ config/mist.mk
@@ -28,3 +28,19 @@ PRODUCT_SYSTEM_EXT_PROPERTIES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/android.hardware.biometrics.face.xml
 endif
+
+# PIF values
+PRODUCT_PRODUCT_PROPERTIES += \
+    persist.sys.pihooks_MANUFACTURER?=Google \
+    persist.sys.pihooks_BRAND?=google \
+    persist.sys.pihooks_PRODUCT?=tangorpro_beta \
+    persist.sys.pihooks_DEVICE?=tangorpro \
+    persist.sys.pihooks_ID?=BP31.250523.006 \
+    persist.sys.pihooks_RELEASE?=16 \
+    persist.sys.pihooks_SECURITY_PATCH?=2025-05-05 \
+    persist.sys.pihooks_DEVICE_INITIAL_SDK_INT?=21 \
+    persist.sys.pihooks_SDK_INT?=35
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PihooksGmsFp="google/tangorpro_beta/tangorpro:16/BP31.250523.006/13607978:user/release-keys" \
+    PihooksGmsModel="Pixel Tablet"
