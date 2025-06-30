3bba99272e58a2cb3a88e10611a37cf5b82edcff
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-06-30 11:10:20 +0000

    vendor: Init BoostFramework changes
    Change-Id: I2184344617e43ad2047827fa54aa365d432b5827

--- config/mist.mk
+++ config/mist.mk
@@ -83,3 +83,34 @@ PRODUCT_PRODUCT_PROPERTIES += \
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PihooksGmsFp="google/tangorpro_beta/tangorpro:16/BP31.250523.006/13607978:user/release-keys" \
     PihooksGmsModel="Pixel Tablet"
+
+AXION_CPU_SMALL_CORES ?= 0,1,2,3
+AXION_CPU_BIG_CORES ?= 4,5,6,7
+AXION_CPU_UNLIMIT_UI ?= 0-7
+AXION_CPU_BG ?= 0-2
+AXION_CPU_FG ?= 0-7
+AXION_CPU_LIMIT_BG ?= 0-1
+AXION_CPU_LIMIT_UI ?= 0-4
+AXION_CPU_DISPLAY ?= 4-7
+AXION_CPU_AUDIO ?= 0-3
+
+DEX2OAT_CORES ?= 0,1,2,3,4,5,6
+
+# AxionOS properties
+PRODUCT_SYSTEM_PROPERTIES += \
+    persist.sys.axion_cpu_big=$(AXION_CPU_BIG_CORES) \
+    persist.sys.axion_cpu_small=$(AXION_CPU_SMALL_CORES) \
+    persist.sys.axion_cpu_bg=$(AXION_CPU_BG) \
+    persist.sys.axion_cpu_limit_bg=$(AXION_CPU_LIMIT_BG) \
+    persist.sys.axion_cpu_fg=$(AXION_CPU_FG) \
+    persist.sys.axion_cpu_limit_ui=$(AXION_CPU_LIMIT_UI) \
+    persist.sys.axion_cpu_unlimit_ui=$(AXION_CPU_UNLIMIT_UI) \
+    persist.sys.axion_cpu_audio=$(AXION_CPU_AUDIO) \
+    persist.sys.axion_cpu_display=$(AXION_CPU_DISPLAY)
+
+# dex2oat
+PRODUCT_SYSTEM_PROPERTIES += \
+    dalvik.vm.dex2oat-threads=6 \
+    dalvik.vm.restore-dex2oat-threads=6 \
+    dalvik.vm.dex2oat-cpu-set=$(DEX2OAT_CORES) \
+    dalvik.vm.restore-dex2oat-cpu-set=$(DEX2OAT_CORES)
