1290e49ea23e04890a62cea1463d698634ecf842
Aaron Kling <webgeek1234@gmail.com>
2025-07-21 00:32:21 -0500

    common: Allow vendors to exclude bp paths
    This resolves a couple issues. Allowing bp's that don't resolve to be
    in-tree. And allowing multiple copies of the same target.
    
    Change-Id: I85cfc566e18ea9fe1cb4cd8d70a71ef80517fd41

--- config/common.mk
+++ config/common.mk
@@ -7,6 +7,9 @@ $(call inherit-product, vendor/google/overlays/ThemeIcons/config.mk)
 
 PRODUCT_BRAND ?= MistOS
 
+# Allow vendor prebuilt repos to exclude themselves from bp scanning
+-include $(sort $(wildcard vendor/*/*/exclude-bp.mk))
+
 ifeq ($(PRODUCT_GMS_CLIENTID_BASE),)
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.com.google.clientidbase=android-google
