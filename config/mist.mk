ae59a33cc4ac0ded449d7a909e178215bd276210
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-09-11 14:23:58 +0000

    config: Add PERF_ANIM_OVERRIDE flag
    Change-Id: I7b40a15a61e60c61ddb3b424d90cfa9d390c17bd

--- config/mist.mk
+++ config/mist.mk
@@ -147,6 +147,7 @@ PRODUCT_PACKAGES += \
 
 # Bypass Charging
 BYPASS_CHARGE_SUPPORTED ?= false
+PERF_ANIM_OVERRIDE ?= false
 
 MIST_CPU_SMALL_CORES ?= 0,1,2,3
 MIST_CPU_BIG_CORES ?= 4,5,6,7
@@ -181,7 +182,8 @@ PRODUCT_SYSTEM_PROPERTIES += \
     persist.sys.axion_cpu_limit_ui=$(MIST_CPU_LIMIT_UI) \
     persist.sys.axion_cpu_unlimit_ui=$(MIST_ALL_CORES) \
     persist.sys.axion_cpu_display=$(MIST_CPU_DISPLAY) \
-    persist.sys.battery_bypass_supported=$(BYPASS_CHARGE_SUPPORTED)
+    persist.sys.battery_bypass_supported=$(BYPASS_CHARGE_SUPPORTED) \
+    persist.sys.activity_anim_perf_override=$(PERF_ANIM_OVERRIDE)
 
 # dex2oat
 PRODUCT_SYSTEM_PROPERTIES += \
