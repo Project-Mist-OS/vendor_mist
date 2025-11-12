4901dddb8f833121595b1ad6c8979214786bae89
Zabuka_zuzu <zuzu950444@gmail.com>
2025-11-12 20:42:32 +0000

    vendor: bump version 4.3 `Rime`

--- config/version.mk
+++ config/version.mk
@@ -2,10 +2,10 @@ PRODUCT_VERSION_MAJOR = 16
 PRODUCT_VERSION_MINOR = 0
 
 # Increase Mist Version with each major release.
-MIST_VERSION_DISPLAY := 4.3-Enma
+MIST_VERSION_DISPLAY := 4.3-Rime
 MIST_FLAVOR := Baklava
 MIST_VERSION_BASE := 4.3
-MIST_CODENAME := Enma
+MIST_CODENAME := Rime
 MIST_BUILD_TYPE ?= Unofficial
 
 MIST_BUILD_DATE := $(shell date -u +%Y%m%d)
