11c80424767ba0b25762a22f7a58a2f9ead3a662
Zabuka_zuzu <zuzu950444@gmail.com>
2025-12-22 19:29:39 +0000

    vendor: bump version 4.4 `Zephyr`

--- config/version.mk
+++ config/version.mk
@@ -2,10 +2,10 @@ PRODUCT_VERSION_MAJOR = 16
 PRODUCT_VERSION_MINOR = 0
 
 # Increase Mist Version with each major release.
-MIST_VERSION_DISPLAY := 4.3-Rime
+MIST_VERSION_DISPLAY := 4.4-Zephyr
 MIST_FLAVOR := Baklava
-MIST_VERSION_BASE := 4.3
-MIST_CODENAME := Rime
+MIST_VERSION_BASE := 4.4
+MIST_CODENAME := Zephyr
 MIST_BUILD_TYPE ?= Unofficial
 
 MIST_BUILD_DATE := $(shell date -u +%Y%m%d)
