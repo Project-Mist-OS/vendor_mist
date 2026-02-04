bd88c7a978324318e041620106a9c2e8716a4440
Ghosuto <clash.raja10@gmail.com>
2026-02-04 10:26:24 +0000

    config: UDFPS customization prop

--- config/mist.mk
+++ config/mist.mk
@@ -178,3 +178,7 @@ PRODUCT_PRODUCT_PROPERTIES += \
 
 PRODUCT_PRODUCT_PROPERTIES += \
     persist.sys.display_refresh_rates_list=$(TARGET_SUPPORTED_REFRESH_RATES)
+
+# UDFPS properties
+PRODUCT_SYSTEM_PROPERTIES += \
+    mist_udfps_custom=$(TARGET_HAS_UDFPS)
