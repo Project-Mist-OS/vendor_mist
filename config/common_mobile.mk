a2bd69582f1b26c3ea196b285a45613672af60d7
Ghosuto <clash.raja10@gmail.com>
2026-02-05 10:34:02 +0000

    vendor: Drop lineage charger animation
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- config/common_mobile.mk
+++ config/common_mobile.mk
@@ -36,16 +36,6 @@ endif
 PRODUCT_PACKAGES += \
     Launcher3Overlay
 
-# Charger
-PRODUCT_PACKAGES += \
-    charger_res_images
-
-ifneq ($(WITH_LINEAGE_CHARGER),false)
-PRODUCT_PACKAGES += \
-    lineage_charger_animation \
-    lineage_charger_animation_vendor
-endif
-
 # Legal
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.lineagelegal.url=https://lineageos.org/legal
