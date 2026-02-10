1dcd65a55ee1a9932292dda735e2bd37b38b0b97
Ghosuto <clash.raja10@gmail.com>
2026-02-10 07:58:30 +0000

    config: Add quickswitch props
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- config/mist.mk
+++ config/mist.mk
@@ -207,3 +207,22 @@ PRODUCT_PRODUCT_PROPERTIES += \
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PihooksGmsFp="google/komodo_beta/komodo:16/CP11.251209.007.A1/14691868:user/release-keys" \
     PihooksGmsModel="Pixel 9 Pro XL"
+
+# Quick Switch
+TARGET_DEFAULT_PIXEL_LAUNCHER ?= false
+ifeq ($(WITH_GMS),true)
+ifeq ($(TARGET_DEFAULT_PIXEL_LAUNCHER), true)
+# Pixel Launcher
+PRODUCT_SYSTEM_PROPERTIES += \
+    persist.sys.default_launcher=1 \
+    persist.sys.quickswitch_pixel_shipped=1
+else
+# Launcher3
+PRODUCT_SYSTEM_PROPERTIES += \
+    persist.sys.default_launcher=0 \
+    persist.sys.quickswitch_pixel_shipped=1
+endif
+else
+PRODUCT_SYSTEM_PROPERTIES += \
+    persist.sys.default_launcher=0
+endif
