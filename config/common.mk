12f41fb864261b15381b02857c25268a8e6fc958
Zabuka_zuzu <zuzu950444@gmail.com>
2026-02-19 19:15:44 +0000

    vendor: adding gapps vendor deps

--- config/common.mk
+++ config/common.mk
@@ -289,3 +289,16 @@ include vendor/lineage/config/version.mk
 
 -include $(WORKSPACE)/build_env/image-auto-bits.mk
 -include vendor/lineage/config/partner_gms.mk
+
+# GMS
+WITH_GMS ?= false
+ifeq ($(WITH_GMS),true)
+    # GAPPS BUILD
+    $(call inherit-product-if-exists, vendor/gapps/permissions/permissions.mk)
+    $(call inherit-product-if-exists, vendor/gapps/arm64/arm64-vendor.mk)
+    MIST_PACKAGE_TYPE := GAPPS
+else
+    # VANILLA BUILD
+    MIST_PACKAGE_TYPE := VANILLA
+endif
+
