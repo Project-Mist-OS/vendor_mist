c3b0b586e01465fea2709faf1d2f49182bd28919
Zabuka_zuzu <zuzu950444@gmail.com>
2026-03-31 19:36:00 +0000

    vendor: Bump up version `4.6-Lucent`

--- config/version.mk
+++ config/version.mk
@@ -2,10 +2,10 @@ PRODUCT_VERSION_MAJOR = 16
 PRODUCT_VERSION_MINOR = 0
 
 # Increase Mist Version with each major release.
-MIST_VERSION_DISPLAY := 4.5-Velour
+MIST_VERSION_DISPLAY := 4.6-Lucent
 MIST_FLAVOR := Baklava
-MIST_VERSION_BASE := 4.5
-MIST_CODENAME := Velour
+MIST_VERSION_BASE := 4.6
+MIST_CODENAME := Lucent
 MIST_BUILD_TYPE ?= Unofficial
 
 MIST_BUILD_DATE := $(shell date -u +%Y%m%d)
