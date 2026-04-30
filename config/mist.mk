3c3b3e83de4abca524ff7680d43d9fbcd7db4bc6
Ghosuto <clash.raja10@gmail.com>
2026-04-30 16:34:25 +0000

    config: gate display engine category on persist prop
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- config/mist.mk
+++ config/mist.mk
@@ -194,10 +194,12 @@ PRODUCT_SYSTEM_EXT_PROPERTIES += \
 BYPASS_CHARGE_SUPPORTED ?= false
 HBM_SUPPORTED ?= false
 HBM_NODE ?= /sys/class/backlight/panel0-backlight/hbm_mode
+USE_REALITY_ENGINE ?= false
 
 PRODUCT_PRODUCT_PROPERTIES += \
     persist.sys.battery_bypass_supported=$(BYPASS_CHARGE_SUPPORTED) \
-    persist.sys.hbmservice_file=$(HBM_NODE)
+    persist.sys.hbmservice_file=$(HBM_NODE) \
+    persist.display.reality.engine.enabled=$(USE_REALITY_ENGINE)
 
 # PIF values
 PRODUCT_PRODUCT_PROPERTIES += \
