0a1a0281f5dbdb3ae3cb4cc30d1969baa247360f
Ghosuto <clash.raja10@gmail.com>
2026-05-07 12:28:00 +0000

    config: Remove PIF data

--- config/mist.mk
+++ config/mist.mk
@@ -199,22 +199,6 @@ PRODUCT_PRODUCT_PROPERTIES += \
     persist.display.reality.engine.enabled=$(USE_REALITY_ENGINE) \
     persist.sys.udfps.custom=$(TARGET_HAS_UDFPS)
 
-# PIF values
-PRODUCT_PRODUCT_PROPERTIES += \
-    persist.sys.pihooks_MANUFACTURER?=Google \
-    persist.sys.pihooks_BRAND?=google \
-    persist.sys.pihooks_PRODUCT?=blazer_beta \
-    persist.sys.pihooks_DEVICE?=blazer \
-    persist.sys.pihooks_ID?=CP21.260306.017 \
-    persist.sys.pihooks_RELEASE?=16 \
-    persist.sys.pihooks_SECURITY_PATCH?=2026-03-05 \
-    persist.sys.pihooks_DEVICE_INITIAL_SDK_INT?=21 \
-    persist.sys.pihooks_SDK_INT?=32
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PihooksGmsFp="google/blazer_beta/blazer:17/CP21.260306.017/15063635:user/release-keys" \
-    PihooksGmsModel="Pixel 10 Pro"
-
 # Quick Switch
 TARGET_DEFAULT_PIXEL_LAUNCHER ?= true
 ifeq ($(WITH_GMS),true)
