0e2ae3b5e701ee3047db7b01e3f11c22b44906de
Zabuka_zuzu <zuzu950444@gmail.com>
2026-07-30 08:48:32 +0000

    vendor: Added GMS with flags

--- config/version.mk
+++ config/version.mk
@@ -42,12 +42,22 @@ ifeq ($(MIST_BUILD_TYPE), OFFICIAL)
   endif
 endif
 
-
 # GMS
 WITH_GMS ?= false
 ifeq ($(WITH_GMS),true)
-    $(call inherit-product, vendor/gms/products/gms.mk)
+  ifeq ($(TARGET_USES_MINI_GAPPS),true)
+    $(call inherit-product, vendor/gms/gms_mini.mk)
+    $(call inherit-product, vendor/pixel-style/config/common.mk)
+    MIST_PACKAGE_TYPE := MINI
+  else ifeq ($(TARGET_USES_PICO_GAPPS),true)
+    $(call inherit-product, vendor/gms/gms_pico.mk)
+    $(call inherit-product, vendor/pixel-style/config/common.mk)
+    MIST_PACKAGE_TYPE := PICO
+  else
+    $(call inherit-product, vendor/gms/gms_full.mk)
+    $(call inherit-product, vendor/pixel-style/config/common.mk)
     MIST_PACKAGE_TYPE := GAPPS
+  endif
 else
     MIST_PACKAGE_TYPE := VANILLA
 endif
